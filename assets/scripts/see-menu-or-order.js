a6b1928bd953380abbd3883b215075ec9289e567
J-B-21 <evinindiomo@gmail.com>
2025-03-13 00:47:32 +0100

    Optimized my work

--- assets/scripts/see-menu-or-order.js
+++ assets/scripts/see-menu-or-order.js
@@ -1,31 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdown = document.getElementById("jb-dropdown");
-    const arrow = dropdown.querySelector("img");
-    const list = document.getElementById("jb-list");
-    const scrwidth = window.matchMedia("(min-width: 768px)");
+//THE FOLLOWING CODE BELONGS TO J-B
+"use strict";
 
-    function handleDropdownClick() {
-        if (list.style.display === "none" || list.style.display === "") {
-            list.style.display = "flex";
-            arrow.style.transform = "rotate(180deg)";
+document.addEventListener("DOMContentLoaded", function () {
+    const jb_dropdown = document.getElementById("jb-dropdown");
+    const jb_arrow = jb_dropdown.querySelector("img");
+    const jb_list = document.getElementById("jb-list");
+    const jb_scrwidth = window.matchMedia("(min-width: 768px)");
+
+    function jbHandleDropdownClick() {
+        if (jb_list.style.display === "none" || jb_list.style.display === "") {
+            jb_list.style.display = "flex";
+            jb_arrow.style.transform = "rotate(180deg)";
         } else {
-            list.style.display = "none";
-            arrow.style.transform = "rotate(0deg)";
+            jb_list.style.display = "none";
+            jb_arrow.style.transform = "rotate(0deg)";
         }
     }
 
-    function checkScrWidth() {
-        if (scrwidth.matches) {
-            list.style.display = "flex";
-            dropdown.removeEventListener("click", handleDropdownClick);
+    function jbCheckScrWidth() {
+        if (jb_scrwidth.matches) {
+            jb_list.style.display = "flex";
+            jb_dropdown.removeEventListener("click", jbHandleDropdownClick);
         } else {
-            dropdown.addEventListener("click", handleDropdownClick);
-            if (arrow.style.transform === "rotate(0deg)") {
-                list.style.display = "none";
+            jb_dropdown.addEventListener("click", jbHandleDropdownClick);
+            if (jb_arrow.style.transform === "rotate(0deg)") {
+                jb_list.style.display = "none";
             }
         }
     }
 
-    checkScrWidth();
-    window.addEventListener("resize", checkScrWidth);
+    jbCheckScrWidth();
+
+    window.addEventListener("resize", jbCheckScrWidth);
 });
+//THE CODE BELONGING TO J-B ENDS HERE
