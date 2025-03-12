73e04ff2b458dda18e437eb2f3c11afed1bca454
J-B-21 <evinindiomo@gmail.com>
2025-03-12 22:45:57 +0100

    Completed my part!

--- assets/scripts/see-menu-or-order.js
+++ assets/scripts/see-menu-or-order.js
@@ -1,21 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    dropdown = document.getElementById("jb-dropdown")
-    arrow = document.getElementById("jb-arrow")
-    list = document.getElementById("jb-list")
+    const dropdown = document.getElementById("jb-dropdown");
+    const arrow = dropdown.querySelector("img");
+    const list = document.getElementById("jb-list");
+    const scrwidth = window.matchMedia("(min-width: 768px)");
 
-    dropdown.addEventListener("click", () => {
+    function handleDropdownClick() {
         if (list.style.display === "none" || list.style.display === "") {
-            list.style.display = "block"
-            arrow.style.transform = "rotate(180deg)"
+            list.style.display = "flex";
+            arrow.style.transform = "rotate(180deg)";
         } else {
-                list.style.display="none"
-                arrow.style.transform="rotate(0deg)"
+            list.style.display = "none";
+            arrow.style.transform = "rotate(0deg)";
+        }
+    }
+
+    function checkScrWidth() {
+        if (scrwidth.matches) {
+            list.style.display = "flex";
+            dropdown.removeEventListener("click", handleDropdownClick);
+        } else {
+            dropdown.addEventListener("click", handleDropdownClick);
+            if (arrow.style.transform === "rotate(0deg)") {
+                list.style.display = "none";
             }
+        }
+    }
 
-        dropdown.classList.add("jb-bgchange")
-
-        setTimeout(() => {
-            dropdown.classList.remove("jb-bgchange")
-        }, 300);
-    })
-})
+    checkScrWidth();
+    window.addEventListener("resize", checkScrWidth);
+});
